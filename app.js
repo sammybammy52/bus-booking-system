f02110f385d73e0dd3ae4ecc245aff8dbb446375
Sammy Bammy <sammybammystudios@gmail.com>
2022-05-28 18:53:09 +0100

    cors

--- app.js
+++ app.js
@@ -11,10 +11,11 @@ const cors = require('cors');
 const app = express();
 
 // middleware
+app.use(cors());
 app.use(express.static('public'));
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+
 
 // view engine
 app.set('view engine', 'ejs');
@@ -29,8 +30,8 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCr
 
 // general routes
 app.get('*', checkUser);
-app.get('/', (req, res) => res.render('home'));
-app.get('/smoothies', requireAuth, (req, res) => res.render('smoothies'));
+
+//app.get('/smoothies', requireAuth, (req, res) => res.render('smoothies'));
 
 //fragmented routes
 
